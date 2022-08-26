5de0f2e50144633cb00dc22c77b4cef0ccc4f623
unknown <cogjava3161@iiht.tech>
2022-08-26 09:50:21 +0530

    java stream

--- Pratice/src/StreamsPratice.java
+++ Pratice/src/StreamsPratice.java
@@ -5,7 +5,7 @@ public class StreamsPratice {
 
 	public static void main(String[] args) {
 
-		Optional<Integer> sum = List.of(1, 2, 34, 5).stream().reduce((a, b) -> a + b);
+		Optional<Integer> sum = List.of(1, 2, 34, 5).stream().reduce((a, b) -> a + b);;
 		Optional<Integer> product = List.of(1, 2, 34, 5).stream().reduce((a, b) -> a * b);
 		Optional<Integer> minus = List.of(1, 2, 34, 5).stream().reduce((a, b) -> a - b);
 		Optional<Integer> divide = List.of(1, 2, 34, 5).stream().reduce((a, b) -> a / b);
